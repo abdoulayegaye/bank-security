2749cf8f00a67f4e87a55fc736b06e1db4f03ebb
Abdoulaye GAYE <layegaye001@gmail.com>
2022-09-05 19:06:21 +0000

    Premier commit

---
+++ src/main/java/com/bank/security/banksecurity/BankSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bank.security.banksecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bank/security/banksecurity/BankSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.security.banksecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
