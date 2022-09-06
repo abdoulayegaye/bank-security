e2c77faed0bf227115735f64c9925bce5367e7da
Abdoulaye GAYE <layegaye001@gmail.com>
2022-09-06 19:04:44 +0000

    Repository, Service, Controller - Part 2

--- src/main/java/com/bank/security/banksecurity/controller/UserController.java
+++ src/main/java/com/bank/security/banksecurity/controller/UserController.java
@@ -1,11 +1,48 @@
 package com.bank.security.banksecurity.controller;
 
+import com.bank.security.banksecurity.domain.User;
+import com.bank.security.banksecurity.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
+
+    UserService userService;
+
+    @GetMapping
+    public Page<User> getUsers(Pageable pageable) {
+        return userService.getUsers(pageable);
+    }
+
+    @GetMapping("{/id}")
+    public User getUser(@PathVariable("id") Long id) {
+        return userService.getUser(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    //@IsAdmin
+    public User createUser(@Valid @RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("{/id}")
+    //@IsAdmin
+    public User updateUser(@PathVariable("id") Long id, @Valid @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("{/id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+    }
+
 }
--- src/main/java/com/bank/security/banksecurity/service/UserService.java
+++ src/main/java/com/bank/security/banksecurity/service/UserService.java
@@ -1,10 +1,61 @@
 package com.bank.security.banksecurity.service;
 
+import com.bank.security.banksecurity.domain.User;
+import com.bank.security.banksecurity.exception.EntityNotFoundException;
+import com.bank.security.banksecurity.exception.RequestException;
+import com.bank.security.banksecurity.mapping.UserMapper;
+import com.bank.security.banksecurity.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Locale;
 
 @Service
 @AllArgsConstructor
 public class UserService {
+
+    UserRepository userRepository;
+    UserMapper userMapper;
+    MessageSource messageSource;
+
+    @Transactional(readOnly = true)
+    public Page<User> getUsers(Pageable pageable) {
+        return userRepository.findAll(pageable).map(userMapper::toUser);
+    }
+
+    @Transactional(readOnly = true)
+    public User getUser(Long id) {
+        return userMapper.toUser(userRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException(messageSource.getMessage("user.notfound", new Object[]{id},
+                        Locale.getDefault()))));
+    }
+
+    @Transactional
+    public User createUser(User user) {
+        return userMapper.toUser(userRepository.save(userMapper.fromUser(user)));
+    }
+
+    @Transactional
+    public User updateUser(Long id, User user){
+        user.setId(id);
+        return userMapper.toUser(userRepository.save(userMapper.fromUser(user)));
+    }
+
+    @Transactional
+    public void deleteUser(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RequestException(messageSource.getMessage("user.errordeletion", new Object[]{id},
+                    Locale.getDefault()),
+                    HttpStatus.CONFLICT);
+        }
+    }
+
+
 }
